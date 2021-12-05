669dcc968f3f350359369a3cb920bff2f143a786
krobingit <liorobin5@gmail.com>
2021-12-05 21:26:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch,Redirect, Route } from 'react-router-dom';
+import { Login } from './Pages/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Switch>
+        <Route path="/">
+          <Redirect to="/login"/>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+/*
+ <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/forgotPassword">
+          <ForgotPassword />
+        </Route>
+        <Route path="/resetPassword/:id/:token">
+          <ResetPassword />
+        </Route>*/
---
+++ src/Pages/login.js
@@ -0,0 +1,74 @@
+import { Form, Button } from 'semantic-ui-react';
+import * as yup from 'yup';
+import { useFormik } from "formik";
+import { Link } from 'react-router-dom';
+function Login() {
+ const signInSchema =
+  yup.object({
+   email: yup.string().email().required('Please enter your Email'),
+   password: yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/, "Password not matched").required('Please enter your password')
+
+  });
+
+ const { handleChange,handleSubmit, handleBlur,errors, touched, values } = useFormik({
+
+  initialValues:{
+   email: "",
+   password:""
+  },
+  validationSchema: signInSchema,
+  onSubmit: (values) => {
+console.log(values)
+  }
+
+
+ })
+ const formStyles = {
+  background: 'beige',
+  boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37)",
+  width: "28rem",
+  padding: "2rem",
+  borderRadius: "1rem",
+  margin:"0rem 1.5rem"
+ }
+
+return(
+ <section className="loginPage">
+
+  <Form style={formStyles} onSubmit={handleSubmit}>
+   <Form.Input onChange={handleChange} onBlur={handleBlur} value={values.email}
+    error={ errors.email && touched.email && errors.email}
+      fluid
+      label='Email'
+      placeholder='Email'
+    id='email'
+    name="email"
+    type="text"
+    />
+   <Form.Input onChange={handleChange} onBlur={handleBlur} value={values.password}
+          error={ errors.password && touched.password && errors.password}
+      fluid
+      label='Password'
+    placeholder='Password'
+    id="password"
+    name="password"
+    type="password"
+   />
+<div className="signin">
+   <Button type="submit" inverted color='red'>Sign in</Button>
+</div>
+   <section className="FormAction">
+    <p>New here?   <Link to="/register">Sign up</Link></p>
+    <Link to="/forgotPassword">Forgot Password</Link>
+</section>
+  </Form>
+
+
+ </section>
+
+
+)
+
+}
+
+export {Login}
